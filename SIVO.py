93948101ddb0c4e4b9447891d80b16586aff64ab
gvelazcamp <97753660+gvelazcamp@users.noreply.github.com>
2026-06-29 17:18:08 -0300

    Update SIVO.py

--- SIVO.py
+++ SIVO.py
@@ -2064,7 +2064,7 @@ h1, h2, h3, h4, h5, h6 {
     }
     
     .integrations-section {
-        margin-top: -40px !important;
+        margin-top: -65px !important;
         padding-top: 15px !important;
     }
     
